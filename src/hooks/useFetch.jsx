054f6c3a1c48fe2d1738565fb6d3da3337949443
AMIKO <akozmanishvili@gmail.com>
2026-06-28 18:48:05 +0400

    commit: created useFetch hook to get data from API

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,7 +1,35 @@
 import { useState, useEffect } from "react";
 
-const useFetch = () => {
-  return <div>useFetch</div>;
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError(false);
+
+    const controller = new AbortController();
+    async function fetching() {
+      try {
+        const resp = await fetch(url, { signal: controller.signal });
+        const js = resp.json();
+        setData(js);
+      } catch (err) {
+        if (err.message === "AbortError") return;
+        console.error(err.message);
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetching();
+
+    return () => controller.abort();
+  }, [url]);
+
+  return { data, isLoading, error };
 };
 
 export default useFetch;
